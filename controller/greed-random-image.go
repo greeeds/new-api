e6b9523a7c2b5725f15876d49562f5d2b04d9736
greed <greedisgoodlsh@gmail.com>
2024-07-20 22:25:21 +0800

    random

--- controller/greed-random-image.go
+++ controller/greed-random-image.go
@@ -6,7 +6,6 @@ import (
 	"math/rand/v2"
 	"net/http"
 	"one-api/model"
-	"os"
 	"strconv"
 )
 
@@ -20,7 +19,6 @@ func GetGreedRandomImageUrlByNum(c *gin.Context) {
 		num = rand.IntN(int(total))
 	}
 	greedImage, err := model.GetGreedRandomImageUrlByNum(num)
-	// 修正：将greedImage 的url对应图片下载下来返回给前端
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{
 			"success": false,
@@ -45,43 +43,13 @@ func GetGreedRandomImageUrlByNum(c *gin.Context) {
 	}(resp.Body)
 	// 获取响应头中的 Content-Type
 	contentType := resp.Header.Get("Content-Type")
-	// 将图片保存到临时文件
-	tmpFile, err := os.CreateTemp("", "downloaded-*.jpg")
-	if err != nil {
-		c.JSON(http.StatusOK, gin.H{
-			"success": false,
-			"message": "Failed to get temp file",
-		})
-		return
-	}
-	defer func(tmpFile *os.File) {
-		err := tmpFile.Close()
-		if err != nil {
-
-		}
-	}(tmpFile)
-
-	_, err = io.Copy(tmpFile, resp.Body)
-	if err != nil {
-		c.JSON(http.StatusOK, gin.H{
-			"success": false,
-			"message": "Failed to resp image",
-		})
-		return
-	}
-
-	// 将图片内容返回给前端
-	_, err = tmpFile.Seek(0, io.SeekStart)
-	if err != nil {
-		return
-	}
 	c.Header("Content-Type", contentType)
-	if _, err := io.Copy(c.Writer, tmpFile); err != nil {
+	// 将下载的文件流直接写入response流
+	if _, err := io.Copy(c.Writer, resp.Body); err != nil {
 		c.JSON(http.StatusOK, gin.H{
 			"success": false,
 			"message": "Failed to send image",
 		})
 		return
 	}
-	return
 }
