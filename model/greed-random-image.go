0b8b871ca94a3972737d486836f0ca153affb787
greed <greedisgoodlsh@gmail.com>
2024-08-28 15:58:01 +0800

    greed-random-image content-type keyword

--- model/greed-random-image.go
+++ model/greed-random-image.go
@@ -7,7 +7,7 @@ import (
 type GreedImage struct {
 	Id          int    `json:"id" gorm:"primaryKey;autoIncrement"`
 	Url         string `json:"url" gorm:"size:500;unique;comment:url"`
-	Nsfw        uint8  `json:"nsfw" gorm:"type:tinyint;default:0;index;comment:Is Not Safe For Work? 0-涩图 1-R18 2-妹子图 3-jk 4-bs 5-hs 6-luolo"`
+	Nsfw        uint8  `json:"nsfw" gorm:"type:tinyint;default:0;index;comment:Is Not Safe For Work? 0-涩图 1-R18 2-妹子图 3-jk 4-bs 5-hs 6-luoli"`
 	ContentType string `json:"content_type" gorm:"size:50;comment:content-type"`
 	Keyword     string `json:"keyword" gorm:"size:50;index;comment:关键词"`
 }
